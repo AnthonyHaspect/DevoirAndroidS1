fc442b4e348300958b0214535470c4945fcb70c5
AnthonyH <2027425@cegepoutaouais.qc.ca>
2023-02-03 08:58:10 -0500

    PremierCommit Interface fait

---
+++ app/src/main/java/com/anthony2027425/devoir/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.anthony2027425.devoir
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anthonyh2027425.myapplication.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
